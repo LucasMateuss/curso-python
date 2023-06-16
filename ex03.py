28bd01858905978de60afc3f70df67ebd843b165
Lucas Mateus de Souza <lucasmateus290903@gmail.com>
2023-06-16 03:24:22 -0300

    ex03 feito

---
+++ ex03.py
@@ -0,0 +1,12 @@
+num1 = input("Digite o primeiro valor: ")
+num2 = input("Digite o segundo valor: ")
+
+if num1 > num2:
+    print(f'{num1=} é maior que o {num2=}')
+elif num2 > num1:
+        print(f'{num2=} é maior que o {num1=}')
+else:
+        print(f'{num1=} é igual ao {num2=}')
+
+
+
