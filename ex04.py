177bd5d242f21cbc5d0d2caeedddf3b73fd8f7f6
Lucas Mateus de Souza <lucasmateus290903@gmail.com>
2023-06-16 15:28:37 -0300

    ex04 feito

---
+++ ex04.py
@@ -0,0 +1,17 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+idade_int = int(idade)
+
+if nome and idade:
+    print(f"""Seu nome é: {nome}
+          Seu nome invetido é: {nome[::-1]}
+          Seu nome tem {len(nome)} letras
+          A primeira letra do seu nome é {nome[0]}
+          A última letra do seu nome é {nome[-1]}
+          """)
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não contém espaços')
+else:
+    print('Desculpe, você não preencheu os campos corretamente')
