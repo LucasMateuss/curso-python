f8ef475e9f68a89923b59bba8c4e16cfa838ca38
Lucas Mateus de Souza <lucasmateus290903@gmail.com>
2023-06-19 15:11:58 -0300

    ex04 feito

--- ex04.py
+++ ex04.py
@@ -3,12 +3,12 @@ idade = input('Digite sua idade: ')
 idade_int = int(idade)
 
 if nome and idade:
-    print(f"""Seu nome é: {nome}
-          Seu nome invetido é: {nome[::-1]}
-          Seu nome tem {len(nome)} letras
-          A primeira letra do seu nome é {nome[0]}
-          A última letra do seu nome é {nome[-1]}
-          """)
+    print(
+f"""Seu nome é: {nome}
+Seu nome invetido é: {nome[::-1]}
+Seu nome tem {len(nome)} letras
+A primeira letra do seu nome é {nome[0]}
+A última letra do seu nome é {nome[-1]}""")
     if ' ' in nome:
         print('Seu nome contém espaços')
     else:
