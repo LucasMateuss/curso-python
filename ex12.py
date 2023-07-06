d445eb94a6a821ed35c7bb1586a8e571aec8a90d
Lucas Mateus de Souza <lucasmateus290903@gmail.com>
2023-07-06 16:39:08 -0300

    feito os ex11 e ex12

---
+++ ex12.py
@@ -0,0 +1,60 @@
+"""
+Calculo do segundo dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 11
+
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
+
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
+Multiplicar o resultado anterior por 10
+363 * 10 = 3630
+Obter o resto da divisão da conta anterior por 11
+3630 % 11 = 0
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O segundo dígito do CPF é 0
+"""
+count1 = 10
+count2 = 11
+cpf = '74682489070'
+cpf_9_digitos = cpf[:9]
+cpf_10_digitos = cpf[:10]
+soma1 = 0
+soma2 = 0
+digito1 = None
+digito2 = None
+
+for digito in cpf_9_digitos:
+    digito = int(digito)
+    soma1 += count1 * digito
+    count1 -=1
+
+soma_multiplicada_1 = soma1 * 10   
+resto_multiplicacao_1 = soma_multiplicada_1 % 11
+
+digito1 = 0 if resto_multiplicacao_1 > 9 else resto_multiplicacao_1
+
+
+for digito in cpf_10_digitos:
+    digito = int(digito)
+    soma2 += count2 * digito
+    count2 -=1
+
+soma_multiplicada_2 = soma2 * 10   
+resto_multiplicacao_2 = soma_multiplicada_2 % 11
+
+digito2 = 0 if resto_multiplicacao_2 > 9 else resto_multiplicacao_2
+
+cpf_calculado = f'{cpf_9_digitos}{digito1}{digito2}'
+
+print(f'O CPF {cpf} é válido') if cpf_calculado == cpf else print(f'O CPF {cpf} é inválido')
